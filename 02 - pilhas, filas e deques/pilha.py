6e53a1a53540432db9ce9dc8ed955490c34a093b
xxPerFecxx <thiago.amorim20171@gmail.com>
2021-09-27 18:36:41 -0300

    Teste Git
    validador de expressões com pilhas

---
+++ 02 - pilhas, filas e deques/pilha.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+class Pilha:
+    def __init__(self, capacidade):
+        self.__capacidade = capacidade
+        self.__topo = -1
+        self.__valores = np.empty(self.__capacidade, dtype=int)
+
+    def __pilha_cheia(self):
+        if self.__topo == self.__capacidade - 1:
+            return True
+        else:
+            return False
+
+    def __pilha_vazia(self):
+        if self.__topo == -1:
+            return True
+        else:
+            return False
+
+    def empilhar(self, valor):
+        if self.__pilha_cheia():
+            print('A pilha está cheia')
+        else:
+            self.__topo += 1
+            self.__valores[self.__topo] = valor
+
+    def desempilhar(self):
+        if self.__pilha_vazia():
+            print('A pilha está vazia')
+        else:
+            self.__topo -= 1
+
+    def ver_topo(self):
+        if self.__topo != -1:
+            return self.__valores[self.__topo]
+        else:
+            return -1
+
+pilha = Pilha(5)
+
+print(pilha.ver_topo())
+
+pilha.empilhar(1)
+print(pilha.ver_topo())
+
+pilha.empilhar(1)
+pilha.empilhar(2)
+pilha.empilhar(3)
+pilha.empilhar(4)
+
+print(pilha.ver_topo())
+
+pilha.empilhar(6)
